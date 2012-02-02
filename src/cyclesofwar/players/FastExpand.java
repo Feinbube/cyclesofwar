880e5427ff0e80e1f66d8a1c706253be776f38b6
robert.wierschke <wierschke@users.noreply.github.com>
2012-02-02 16:32:36 +0000

    FastExpand player

---
+++ src/cyclesofwar/players/FastExpand.java
@@ -0,0 +1,45 @@
+package cyclesofwar.players;
+
+import java.awt.Color;
+import java.util.Collections;
+import java.util.List;
+
+import javax.sound.sampled.ReverbType;
+
+import cyclesofwar.Planet;
+import cyclesofwar.Player;
+
+
+public class FastExpand extends Player {
+
+	@Override
+	protected void think() {
+		for (Planet  planet : getPlanets()) {
+			List<Planet> targets = getAllPlanetButMine();
+			sortByForceCount(targets);
+			Collections.reverse(targets);
+			
+			while (planet.getForces() > 1 && !targets.isEmpty()) {
+				Planet target = targets.remove(0);
+				sendFleet(planet, target.getForces() + 1, target);
+			}
+		}
+		
+	}
+
+	@Override
+	protected Color getPlayerBackColor() {
+		return Color.red;
+	}
+
+	@Override
+	protected Color getPlayerForeColor() {
+		return Color.orange;
+	}
+
+	@Override
+	protected String getCreatorsName() {
+		return "Robert";
+	}
+
+}
