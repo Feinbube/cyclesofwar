2905c67e2bcdaffaf3baf1ede2cc68862bdde8c0
Frank.Feinbube <Frank.Feinbube@a5f11994-e429-4278-a1ed-1d2f40d43834>
2012-02-02 01:45:57 +0000

    now Petra beats everyone include the idle player :D

--- src/cyclesofwar/players/Petra.java
+++ src/cyclesofwar/players/Petra.java
@@ -15,30 +15,83 @@ public class Petra extends Player {
 	List<Fleet> fleetsHandled = new ArrayList<Fleet>();
 
 	@Override
-	public void think() {
+	public void think() {	
+		if(this.getPlanets().size() == 0)
+			return;
+		
+		boolean attack = false;
 		for (Planet planet : this.getPlanets()) {
-			Planet target = getTarget(planet);
-			if (target == null)
+			List<Planet> targets = getTargets(planet);
+			if (targets.size() == 0)
 				continue;
 
-			if (planet.getForces() > target.getForces() * 1.5)
-				this.sendFleet(planet, (int) (planet.getForces() * 0.8), target);
+			if (planet.getForces() > targets.get(0).getForces() * 1.5) {				
+				this.sendFleet(planet, (int) (planet.getForces() * 0.8), targets.get(0));
+				attack = true;
+			} else if (targets.size() > 1 && planet.getForces() > targets.get(1).getForces() * 2) {
+				this.sendFleet(planet, (int) (planet.getForces() * 0.6), targets.get(1));
+				attack = true;
+			} else if (targets.size() > 2 && planet.getForces() > targets.get(2).getForces() * 3) {
+				this.sendFleet(planet, (int) (planet.getForces() * 0.4), targets.get(2));
+				attack = true;
+			} 
+		}
+		
+		if(attack == false) {
+			int forces = 0;
+			
+			List<Planet> targets = getTargets();
+			if (targets.size() == 0)
+				return;
+			
+			for (Planet planet : this.getPlanets()) {
+				forces += planet.getForces();
+			}
+			
+			if(forces > targets.get(0).getForces()*3) {
+				for (Planet planet : this.getPlanets()) {
+					this.sendFleet(planet, (int) (planet.getForces() * 0.5), targets.get(0));
+				}
+			}
 		}
 	}
 
-	private Planet getTarget(final Planet planet) {
-		List<Planet> targets = mostProductive(notOwnedByMe(getOtherPlanets(planet)));
-		if (targets.size() == 0)
-			return null;
-
-		Collections.sort(targets, new Comparator<Planet>() {
-			@Override
-			public int compare(Planet planet1, Planet planet2) {
-				return (int) (planet.distanceTo(planet1) - planet.distanceTo(planet2));
+	private List<Planet> getTargets() {
+		List<Planet> targets = notOwnedByMe(this.getAllPlanets());
+		List<Planet> result = new ArrayList<Planet>();
+		
+		while(targets.size() > 0) {
+			List<Planet> next = mostProductive(targets);
+			
+			for(Planet productivePlanet : next) {
+				result.add(productivePlanet);
+				targets.remove(productivePlanet);
 			}
-		});
+		}
 
-		return targets.get(0);
+		return result;
+	}
+	
+	private List<Planet> getTargets(final Planet planet) {
+		List<Planet> targets = notOwnedByMe(getOtherPlanets(planet));
+		List<Planet> result = new ArrayList<Planet>();
+		
+		while(targets.size() > 0) {
+			List<Planet> next = mostProductive(targets);
+			Collections.sort(next, new Comparator<Planet>() {
+				@Override
+				public int compare(Planet planet1, Planet planet2) {
+					return (int) (planet.distanceTo(planet1) - planet.distanceTo(planet2));
+				}
+			});
+			
+			for(Planet productivePlanet : next) {
+				result.add(productivePlanet);
+				targets.remove(productivePlanet);
+			}
+		}
+
+		return result;
 	}
 
 	private List<Planet> mostProductive(List<Planet> planets) {
