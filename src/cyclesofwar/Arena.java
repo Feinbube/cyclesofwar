7a90fb0658005cc9fc8385fcf5718117eaa2de7d
Frank Feinbube <Feinbube@users.noreply.github.com>
2012-02-05 15:59:32 +0000

    use fresh players always -> problem with replays seems to be fixed

--- src/cyclesofwar/Arena.java
+++ src/cyclesofwar/Arena.java
@@ -14,45 +14,47 @@ public class Arena {
 	
 	static final int matchesPerPairing = 25;
 	
-	public static List<Player> combatants(){
+	public static List<Player> playersForGameMode(){
 		ArrayList<Player> result = new ArrayList<Player>();
-		
-		// Training
-		result.add(new IdlePlayer());
-		
-		// Martin
+
+		result.add(new Bean());
 		result.add(new Cratters());
-		
-		// Robert
-		//result.add(new AttackLargestPlayer());
-		
-		// Frank
-		//result.add(new Bean());
+		result.add(new AttackLargestPlayer());
 		
 		return result;
 	}
 	
-	public static List<Player> allPlayers(){
+	public static List<Player> playersForArenaMode(){
+		ArrayList<Player> result = new ArrayList<Player>();
+
+		result.add(new Bean());
+		result.add(new Cratters());
+		result.add(new AttackLargestPlayer());
+		
+		return result;
+	}
+	
+	public static List<Player> registeredPlayers(){
 		ArrayList<Player> result = new ArrayList<Player>();
 		
 		// Training
-		//result.add(new IdlePlayer());
-		//result.add(new RandomPlayer());
-		//result.add(new SharpShooter());
+		result.add(new IdlePlayer());
+		result.add(new RandomPlayer());
+		result.add(new SharpShooter());
 		
 		// Martin
 		result.add(new Cratters());
 		
 		// Robert
-		//result.add(new Defender());		
-		//result.add(new FastExpand());
+		result.add(new Defender());		
+		result.add(new FastExpand());
 		result.add(new AttackLargestPlayer());
 		// result.add(new Borg());
 		
 		// Frank
-		//result.add(new Petra());
-		//result.add(new HanTzu());
-		//result.add(new Alai());
+		result.add(new Petra());
+		result.add(new HanTzu());
+		result.add(new Alai());
 		result.add(new Bean());
 		
 		return result;
