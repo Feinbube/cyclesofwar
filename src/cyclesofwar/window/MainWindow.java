397394292178ee79a96f98c3a735653ddd7aea9d
Frank.Feinbube <Frank.Feinbube@a5f11994-e429-4278-a1ed-1d2f40d43834>
2012-02-07 15:52:06 +0000

    neue MainMethode für Console.

--- src/cyclesofwar/window/MainWindow.java
+++ src/cyclesofwar/window/MainWindow.java
@@ -3,51 +3,13 @@ package cyclesofwar.window;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.event.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import javax.swing.JFrame;
 
 import cyclesofwar.Arena;
-import cyclesofwar.Player;
-import cyclesofwar.tournament.LastManStandingTournament;
-import cyclesofwar.tournament.Tournament;
 
 public class MainWindow {
 	public static void main(String[] args) {
-		List<String> parameters = new ArrayList<String>(Arrays.asList(args));
-
-		int threadcount = Runtime.getRuntime().availableProcessors();
-
-		if (parameters.contains("--noGUI")) {
-			startTournament(threadcount);
-		} else {
-			startGUI(threadcount);
-		}
-	}
-
-	private static void startTournament(int threadcount) {
-		List<Player> champions = Arena.champions();
-
-		int place = 0;
-		Tournament tournament = null;
-		while (champions.size() > 1) {
-			tournament = new LastManStandingTournament(threadcount, champions, Arena.matchesInTournament);
-			tournament.runToCompletion();
-			Player winner = tournament.rankedPlayers().get(0);
-			printWinner(++place, tournament, winner);
-			champions.remove(winner);
-		}
-		printWinner(++place, tournament, champions.get(0));
-	}
-
-	private static void printWinner(int place, Tournament tournament, Player winner) {
-		System.out.println(place + "," + winner.getName() + "," + tournament.wonBy(winner).size() + ","
-				+ tournament.participatedIn(winner).size());
-	}
-
-	private static void startGUI(int threads) {
 		JFrame f = new JFrame("Cycles of War");
 		f.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
@@ -55,7 +17,7 @@ public class MainWindow {
 			}
 		});
 		// TODO: remember last session
-		Container panel = new GamePanel(threads, Arena.champions(), Arena.registeredPlayers(), Arena.matchesInTournament);
+		Container panel = new GamePanel(Runtime.getRuntime().availableProcessors(), Arena.champions(), Arena.registeredPlayers(), Arena.matchesInTournament);
 		f.getContentPane().add(panel, BorderLayout.CENTER);
 
 		f.addKeyListener((KeyListener) panel);
