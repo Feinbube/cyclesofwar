2bb7c4b56e0f6cc7be42a7566c03fb849c696804
troeger <troeger@a5f11994-e429-4278-a1ed-1d2f40d43834>
2012-02-08 14:21:14 +0000

    Peter is in the game

---
+++ src/cyclesofwar/players/peter/ViralForces.java
@@ -0,0 +1,47 @@
+package cyclesofwar.players.peter;
+import java.awt.Color;
+import java.util.Collections;
+import java.util.List;
+
+import cyclesofwar.Fleet;
+import cyclesofwar.Planet;
+import cyclesofwar.Player;
+
+
+public class ViralForces extends Player {
+
+	@Override
+	protected void think() {
+		int maxNeigbours = 5;
+		int looserFactor = 2;
+		for (Planet  planet : getPlanets()) {
+			List<Planet> targets = getAllPlanetsButMine();
+			if (targets.size() >= maxNeigbours) {
+				sortByDistanceTo(targets, planet);
+				for (int i=0; i<maxNeigbours; i++) {
+					Planet target = targets.get(i);		
+					if (target.getForces()*looserFactor < planet.getForces()) {
+						sendFleetUpTo(planet, 99, target);
+						break;
+					}
+				}
+			}
+		}
+	}
+	
+	@Override
+	public Color getPlayerBackColor() {
+		return Color.green.darker().darker();
+	}
+
+	@Override
+	public Color getPlayerForeColor() {
+		return Color.black;
+	}
+
+	@Override
+	public String getCreatorsName() {
+		return "Peter";
+	}
+
+}
