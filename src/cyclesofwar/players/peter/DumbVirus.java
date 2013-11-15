416bee0af221f23cac43d22a7ba923c1440f5a42
troeger <troeger@a5f11994-e429-4278-a1ed-1d2f40d43834>
2013-11-15 08:22:42 +0000

    More dumbness

--- src/cyclesofwar/players/peter/DumbVirus.java
+++ src/cyclesofwar/players/peter/DumbVirus.java
@@ -4,23 +4,33 @@ import java.util.List;
 
 import cyclesofwar.Planet;
 import cyclesofwar.Player;
-
+import cyclesofwar.Fleet;
 
 public class DumbVirus extends Player {
 
 	@Override
 	protected void think() {
-		for (Planet  planet : getPlanets()) {
-			List<Planet> targets = getAllPlanetsButMine();
-			if (targets.size() > 0) {
-				planet.sortOthersByDistance(targets);
-				if (targets.size() > 0) {
-					if (targets.get(0).getProductionRatePerRound() > planet.getProductionRatePerRound())
-						// cool, mehr Futter, umziehen - allerdings mit Anti-Dumm-Bot-Logik
-						sendFleetUpTo(planet, planet.getForces()-1, targets.get(0));
-					else
-						// nur vermehren
-						sendFleetUpTo(planet, planet.getForces()/2, targets.get(0));
+		// TODO: make this dynamic, according to the number of enemy fleets expected
+		double maxSendFleetFactor = 0.5;		// never send more than this part of the home fleet
+		double targetOverrideFactor = 0.1;		// to which factor do we want to send more fleets than the target has
+		int maxNeighbours = 3;					// restrict on the close environment of these many planets
+		// go through my own planets
+		for (Planet  home : getPlanets()) {
+			long maxToBeMoved = java.lang.Math.round(maxSendFleetFactor*home.getForces());
+			List<Planet> neighbours = home.getOthersByDistance();
+			for (int i=0; i<maxNeighbours; i++) {
+				Planet target = neighbours.get(i);
+				long toBeSend = java.lang.Math.round(targetOverrideFactor*target.getForces() + target.getForces());
+				Fleet f;
+				if (target.isFree() && target.getForces() < maxToBeMoved) {
+					f = sendFleetUpTo(home, maxToBeMoved, target);
+					f.setFormation(Fleet.Formation.O);
+					break;
+				}
+				else if (toBeSend < maxToBeMoved) {
+					f = sendFleetUpTo(home, toBeSend, target);
+					// f.setFormation(Fleet.Formation.ARROW);    Leads to NULL Pointer Exception
+					break;
 				}
 			}
 		}
@@ -40,5 +50,4 @@ public class DumbVirus extends Player {
 	public String getCreatorsName() {
 		return "Peter";
 	}
-
 }
